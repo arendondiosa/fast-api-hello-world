d4f47f302d3fab00a5723f291fa3b410abd06549
Alejandro E. Rendon <alejandro@rendon.co>
2022-01-09 11:56:08 -0500

    ✨ request response body

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"hello": "world"}
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
